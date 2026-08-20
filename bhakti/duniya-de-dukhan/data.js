eb0567ec119ef4945ae8a826154a73f63dbdc1df
Eeshan <eeshan@mit.edu>
2026-08-20 00:26:44 -0400

    Song: add Duniya De Dukhan Menu Bada Hi Sataya Maa

---
+++ bhakti/duniya-de-dukhan/data.js
@@ -0,0 +1,282 @@
+/* =============================================================
+   Duniya De Dukhan Menu Bada Hi Sataya Maa — song data
+   ============================================================= */
+
+window.SONG_LINES = {
+  invocation: {
+    roman: "Jai karā sherān wālī maiyā jī dā... Bol sache darbār kī jai",
+    english: "{0:Hail} to {3:Mother} {1,2:who rides the lion}... {4:Chant}: {8:Glory} {6:to the} {7:true court}!",
+    words: [
+      { roman: "Jai karā", gloss: "Hail, victory chant" },
+      { roman: "sherān", gloss: "lions" },
+      { roman: "wālī", gloss: "bearer of, rider of" },
+      { roman: "maiyā", gloss: "Mother" },
+      { roman: "jī", gloss: "honorific" },
+      { roman: "dā", gloss: "of" },
+      { roman: "Bol", gloss: "Speak, chant" },
+      { roman: "sache", gloss: "true" },
+      { roman: "darbār", gloss: "court, temple" },
+      { roman: "kī", gloss: "of" },
+      { roman: "jai", gloss: "victory, glory" }
+    ]
+  },
+
+  refrain_a: {
+    roman: "Duniyā de dukhān mainū baḍā hī satāyā mā",
+    english: "The {0:world's} {2:sorrows} {3:have} {4,5:greatly} {6:tormented} {3:me}, {7:Mother}.",
+    words: [
+      { roman: "Duniyā", gloss: "world" },
+      { roman: "de", gloss: "of" },
+      { roman: "dukhān", gloss: "sorrows" },
+      { roman: "mainū", gloss: "me" },
+      { roman: "baḍā", gloss: "very, greatly" },
+      { roman: "hī", gloss: "only, indeed (emphatic)" },
+      { roman: "satāyā", gloss: "tormented, troubled" },
+      { roman: "mā", gloss: "Mother" }
+    ]
+  },
+
+  refrain_b: {
+    roman: "Jad vī terī loṛ paī, tere kol āyā mā",
+    english: "{0,1:Whenever} {4:the need} {2:for You} {3:arose}, {7:I came} {6:to} {5:You}, {8:Mother}.",
+    words: [
+      { roman: "Jad", gloss: "when" },
+      { roman: "vī", gloss: "ever, also" },
+      { roman: "terī", gloss: "Your" },
+      { roman: "loṛ", gloss: "need" },
+      { roman: "paī", gloss: "fell, arose" },
+      { roman: "tere", gloss: "Your" },
+      { roman: "kol", gloss: "near, to" },
+      { roman: "āyā", gloss: "came" },
+      { roman: "mā", gloss: "Mother" }
+    ]
+  },
+
+  v1a: {
+    roman: "Māwān wālā pyār bas jag 'ch anokhā ae",
+    english: "A {0:mother's} {2:love} {7:is} the {3:only} {6:unique} thing {5:in} this {4:world}.",
+    words: [
+      { roman: "Māwān", gloss: "mothers" },
+      { roman: "wālā", gloss: "associated with, of" },
+      { roman: "pyār", gloss: "love" },
+      { roman: "bas", gloss: "only, just" },
+      { roman: "jag", gloss: "world" },
+      { roman: "'ch", gloss: "in (contraction of vicc)" },
+      { roman: "anokhā", gloss: "unique" },
+      { roman: "ae", gloss: "is" }
+    ]
+  },
+
+  v1b: {
+    roman: "Bākī sārā zindagī 'ch dhokhā hī dhokhā ae",
+    english: "{0:All} the {1:rest} {3:in} {2:life} {7:is} {5,6:nothing but} {4:betrayal}.",
+    words: [
+      { roman: "Bākī", gloss: "rest" },
+      { roman: "sārā", gloss: "all" },
+      { roman: "zindagī", gloss: "life" },
+      { roman: "'ch", gloss: "in" },
+      { roman: "dhokhā", gloss: "betrayal, deceit" },
+      { roman: "hī", gloss: "only (emphatic)" },
+      { roman: "dhokhā", gloss: "betrayal" },
+      { roman: "ae", gloss: "is" }
+    ]
+  },
+
+  v1c: {
+    roman: "Jad vī main hasseyā, mainū apneyān ruwāyā mā",
+    english: "{0,1:Whenever} {2:I} {3:laughed}, {5:my own people} {6:made} {4:me} {6:cry}, {7:Mother}.",
+    words: [
+      { roman: "Jad", gloss: "when" },
+      { roman: "vī", gloss: "ever" },
+      { roman: "main", gloss: "I" },
+      { roman: "hasseyā", gloss: "laughed" },
+      { roman: "mainū", gloss: "me" },
+      { roman: "apneyān", gloss: "my own (people)" },
+      { roman: "ruwāyā", gloss: "made to cry" },
+      { roman: "mā", gloss: "Mother" }
+    ]
+  },
+
+  v2a: {
+    roman: "Ik vārī maiyā merī bigḍī sawār de",
+    english: "{2:Mother}, {5,6:fix} {3:my} {4:ruined situation} just {0,1:once}.",
+    words: [
+      { roman: "Ik", gloss: "one" },
+      { roman: "vārī", gloss: "time" },
+      { roman: "maiyā", gloss: "Mother" },
+      { roman: "merī", gloss: "my" },
+      { roman: "bigḍī", gloss: "ruined (state)" },
+      { roman: "sawār", gloss: "fix, repair" },
+      { roman: "de", gloss: "give (auxiliary verb)" }
+    ]
+  },
+
+  v2b: {
+    roman: "Patthar baṇā de sānū sohṇe darbār de",
+    english: "{1,2:Make} {3:me} a {0:stone} {6:of} Your {4:beautiful} {5:court}.",
+    words: [
+      { roman: "Patthar", gloss: "stone" },
+      { roman: "baṇā", gloss: "make" },
+      { roman: "de", gloss: "give (auxiliary)" },
+      { roman: "sānū", gloss: "us (me)" },
+      { roman: "sohṇe", gloss: "beautiful" },
+      { roman: "darbār", gloss: "court, temple" },
+      { roman: "de", gloss: "of" }
+    ]
+  },
+
+  v2c: {
+    roman: "Bhull chukk terā pair, sāḍe utte āyā mā",
+    english: "So that {2:Your} {3:foot} {6:may come} {5:upon} {4:me} {0,1:by mistake}, {7:Mother}.",
+    words: [
+      { roman: "Bhull", gloss: "forgetfulness, error" },
+      { roman: "chukk", gloss: "mistake" },
+      { roman: "terā", gloss: "Your" },
+      { roman: "pair", gloss: "foot" },
+      { roman: "sāḍe", gloss: "us (me)" },
+      { roman: "utte", gloss: "upon" },
+      { roman: "āyā", gloss: "came" },
+      { roman: "mā", gloss: "Mother" }
+    ]
+  },
+
+  v3a: {
+    roman: "Jad vī tū denī ae mā, nā de ke jatānī ae",
+    english: "{0,1:Whenever} {2:You} {3,4:give}, {5:Mother}, {2:You} {6,9:do not} {8:boast} {7:after giving}.",
+    words: [
+      { roman: "Jad", gloss: "when" },
+      { roman: "vī", gloss: "ever" },
+      { roman: "tū", gloss: "You" },
+      { roman: "denī", gloss: "give" },
+      { roman: "ae", gloss: "do" },
+      { roman: "mā", gloss: "Mother" },
+      { roman: "nā", gloss: "not" },
+      { roman: "de ke", gloss: "having given" },
+      { roman: "jatānī", gloss: "show off, remind" },
+      { roman: "ae", gloss: "do" }
+    ]
+  },
+
+  v3b: {
+    roman: "Sab bachcheyān dī maiyā lāj bachānī ae",
+    english: "{3:Mother}, {2:You} {5,6:save} the {4:honor} {2:of} {0:all} {1:children}.",
+    words: [
+      { roman: "Sab", gloss: "all" },
+      { roman: "bachcheyān", gloss: "children" },
+      { roman: "dī", gloss: "of" },
+      { roman: "maiyā", gloss: "Mother" },
+      { roman: "lāj", gloss: "honor, grace" },
+      { roman: "bachānī", gloss: "save, protect" },
+      { roman: "ae", gloss: "do" }
+    ]
+  },
+
+  v3c: {
+    roman: "Taiyon tainū maiyā rāṇī... apṇā banāyā mā",
+    english: "{0:That's why}, {2:Mother} {3:Queen}, I have {5:made} {1:You} my {4:own}, {6:Mother}.",
+    words: [
+      { roman: "Taiyon", gloss: "that's why, therefore" },
+      { roman: "tainū", gloss: "You" },
+      { roman: "maiyā", gloss: "Mother" },
+      { roman: "rāṇī", gloss: "Queen" },
+      { roman: "apṇā", gloss: "own" },
+      { roman: "banāyā", gloss: "made" },
+      { roman: "mā", gloss: "Mother" }
+    ]
+  },
+
+  v4a: {
+    roman: "Ik vārī maiyā sāḍī jholī nū bhar de",
+    english: "{2:Mother}, {6,7:fill} {3:my} {4:lap} just {0,1:once}.",
+    words: [
+      { roman: "Ik", gloss: "one" },
+      { roman: "vārī", gloss: "time" },
+      { roman: "maiyā", gloss: "Mother" },
+      { roman: "sāḍī", gloss: "our (my)" },
+      { roman: "jholī", gloss: "lap, alms bag" },
+      { roman: "nū", gloss: "to (accusative)" },
+      { roman: "bhar", gloss: "fill" },
+      { roman: "de", gloss: "give" }
+    ]
+  },
+
+  v4b: {
+    roman: "Godī 'ch biṭhā ke sānū māwān wālā pyār de",
+    english: "{2,3:Having seated} {4:me} {1:in} Your {0:lap}, {8:give} me a {5,6:mother's} {7:love}.",
+    words: [
+      { roman: "Godī", gloss: "lap" },
+      { roman: "'ch", gloss: "in" },
+      { roman: "biṭhā", gloss: "seat" },
+      { roman: "ke", gloss: "having" },
+      { roman: "sānū", gloss: "us (me)" },
+      { roman: "māwān", gloss: "mothers" },
+      { roman: "wālā", gloss: "associated with" },
+      { roman: "pyār", gloss: "love" },
+      { roman: "de", gloss: "give" }
+    ]
+  },
+
+  v4c: {
+    roman: "Duniyā 'ch koī mere... kamm nahīyo āyā mā",
+    english: "{2:No one} {1:in} the {0:world} {6:came} {5:to} {3:my} {4:use}, {7:Mother}.",
+    words: [
+      { roman: "Duniyā", gloss: "world" },
+      { roman: "'ch", gloss: "in" },
+      { roman: "koī", gloss: "anyone" },
+      { roman: "mere", gloss: "my" },
+      { roman: "kamm", gloss: "use, work" },
+      { roman: "nahīyo", gloss: "not" },
+      { roman: "āyā", gloss: "came" },
+      { roman: "mā", gloss: "Mother" }
+    ]
+  }
+};
+
+window.SONG_SEQUENCE = [
+  { ref: "invocation" },
+  { ref: "refrain_a", repeats: 6 },
+  { ref: "refrain_b" },
+  { ref: "refrain_a", repeats: 4 },
+  { ref: "v1a" },
+  { ref: "v1b", repeats: 3 },
+  { ref: "v1c", repeats: 2 },
+  { ref: "refrain_a", repeats: 4 },
+  { ref: "v2a" },
+  { ref: "v2b", repeats: 3 },
+  { ref: "v2c", repeats: 3 },
+  { ref: "refrain_a", repeats: 4 },
+  { ref: "v3a" },
+  { ref: "v3b", repeats: 3 },
+  { ref: "v3c", repeats: 4 },
+  { ref: "refrain_a", repeats: 4 },
+  { ref: "v4a" },
+  { ref: "v4b", repeats: 3 },
+  { ref: "v4c", repeats: 4 },
+  { ref: "refrain_a", repeats: 4 },
+  { ref: "refrain_b", repeats: 3 },
+  { ref: "refrain_a", repeats: 7 }
+];
+
+window.SONG_TIMINGS = [
+  { start: 0.00, end: 44.00 },    // refrain_a x6
+  { start: 44.00, end: 55.00 },    // refrain_b
+  { start: 55.00, end: 83.00 },   // refrain_a x4
+  { start: 106.00, end: 113.50 },  // v1a
+  { start: 113.50, end: 133.50 },  // v1b x3
+  { start: 133.50, end: 143.00 },  // v1c x2
+  { start: 143.00, end: 171.00 },  // refrain_a x4
+  { start: 171.00, end: 178.00 },  // v2a
+  { start: 178.00, end: 197.50 },  // v2b x3
+  { start: 197.50, end: 214.00 },  // v2c x3
+  { start: 214.00, end: 242.00 },  // refrain_a x4
+  { start: 265.50, end: 272.00 },  // v3a
+  { start: 272.00, end: 292.00 },  // v3b x3
+  { start: 292.00, end: 308.50 },  // v3c x4
+  { start: 308.50, end: 336.00 },  // refrain_a x4
+  { start: 336.00, end: 342.00 },  // v4a
+  { start: 342.00, end: 362.00 },  // v4b x3
+  { start: 362.00, end: 378.00 },  // v4c x4
+  { start: 378.00, end: 404.00 },  // refrain_a x4
+  { start: 404.00, end: 413.00 },  // refrain_b x3
+  { start: 413.00, end: 514.00 }   // refrain_a x7
+];
